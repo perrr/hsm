48db2fdfdd67855d06d7784747eef390fbbb2f8a
perod <perod@stud.ntnu.no>
2014-12-06 21:31:12 +0100

    add lists of athletes, clubs and nations

--- web/club.php
+++ web/club.php
@@ -6,6 +6,8 @@ $id = mysqli_real_escape_string($conn, $_GET['id']);
 if (is_numeric($id)) {
 	$sqlC = 'SELECT * FROM club WHERE id = '.$id;
 	$club = $conn->query($sqlC);
+	$sqlA = 'SELECT id, first_name, last_name FROM athlete WHERE club = '.$id;
+	$athletes = $conn->query($sqlA);
 
 	if ($club->num_rows > 0) {
 		$rowC = $club->fetch_assoc();
@@ -17,6 +19,12 @@ if (is_numeric($id)) {
 		.'<tr><td class="l">Nation</td><td class="r"><a href="index.php?element=nation.php&id='.$rowC['nation'].'">'.$rowN['name'].'</a></td></tr>'
 		.'<tr><td class="l">Manager</td><td class="r">'.$rowC['manager'].'</td></tr><tr><td></td><td class="r">';
 		echo ($rowC['active'] == 1 ? 'Active' : 'Inactive').'</td></tr></table>';
+		if ($athletes->num_rows > 0) {
+			echo '<h2>Athletes</h1>';
+			while ($row = $athletes->fetch_assoc()) {
+				echo '<a href="index.php?element=athlete.php&id='.$row['id'].'">'.$row['first_name'].' '.$row['last_name'].'</a><br />';
+			}
+		}
 	}
 	else {
 		echo 'Club does not exist';
@@ -28,7 +36,7 @@ else {
 	$result = $conn->query($sqlH);
 
 	if ($result->num_rows > 0) {
-		while($row = $result->fetch_assoc()) {
+		while ($row = $result->fetch_assoc()) {
 			echo '<a href="index.php?element=club.php&id='.$row['id'].'">'.$row['name'].'</a><br />';
 		}
 	}
--- web/nation.php
+++ web/nation.php
@@ -6,6 +6,12 @@ $id = mysqli_real_escape_string($conn, $_GET['id']);
 if (is_numeric($id)) {
 	$sqlN = 'SELECT * FROM nation WHERE id = '.$id;
 	$nation = $conn->query($sqlN);
+	$sqlA = 'SELECT id, first_name, last_name FROM athlete WHERE nation = '.$id;
+	$athletes = $conn->query($sqlA);
+	$sqlC = 'SELECT id, name FROM club WHERE nation = '.$id;
+	$clubs = $conn->query($sqlC);
+	$sqlH = 'SELECT id, name, hs FROM hill WHERE nation = '.$id;
+	$hills = $conn->query($sqlH);
 
 	if ($nation->num_rows > 0) {
 		$rowN = $nation->fetch_assoc();
@@ -13,6 +19,24 @@ if (is_numeric($id)) {
 		echo '<table class="stats-table"><tr><td class="l">ID</td><td class="r">'.$rowN['id'].'</td></tr>'
 		.'<tr><td class="l">Abbrevation</td><td class="r">'.$rowN['abbrevation'].'</td></tr>'
 		.'<tr><td class="l">National record</td><td class="r">'.$rowN['national_record'].' m</td></tr></table>';
+		if ($athletes->num_rows > 0) {
+			echo '<h2>Athletes</h1>';
+			while ($row = $athletes->fetch_assoc()) {
+				echo '<a href="index.php?element=athlete.php&id='.$row['id'].'">'.$row['first_name'].' '.$row['last_name'].'</a><br />';
+			}
+		}
+		if ($clubs->num_rows > 0) {
+			echo '<h2>Clubs</h1>';
+			while ($row = $clubs->fetch_assoc()) {
+				echo '<a href="index.php?element=club.php&id='.$row['id'].'">'.$row['name'].'</a><br />';
+			}
+		}
+		if ($hills->num_rows > 0) {
+			echo '<h2>Hills</h1>';
+			while ($row = $hills->fetch_assoc()) {
+				echo '<a href="index.php?element=hill.php&id='.$row['id'].'">'.$row['name'].' (HS '.$row['hs'].')</a><br />';
+			}
+		}
 	}
 	else {
 		echo 'Nation does not exist';
